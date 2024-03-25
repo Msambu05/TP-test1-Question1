055a1e509bcdf6fb518a12a54e4361b7d562fdc5
Msambu05 <sambuloseluleko05@gmail.com>
2024-03-25 11:53:00 +0200

    Add to my git Repository

--- register.py
+++ register.py
@@ -7,7 +7,7 @@ def register_party(parties, new_party):
     new_member_count = new_party.get("members")
 
     # Check if the member count isacceptable
-    if min_members <= new_member_count:
+    if min_members >= new_member_count:
         # If acceptable, add the new party to the list of parties
         parties.append(new_party)
         print(f"Party '{new_party['party_name']}' registered successfully!")
@@ -21,12 +21,48 @@ parties = [
      {"party_name": "IF", "reg_number": 10003318, "members": 2500}
 
 
-]  # Define an empty list to hold parties
+]  
+
 new_party = {"party_name": "MK", "reg_number": 10003319, "members": 5300}
 
 register_party(parties, new_party)
 
+# List of parties and their abbreviations
+parties = ["ANC", "DA", "EFF", "IFP", "UDM", "COPE","ATM", "NFP",]
 
+#2.4
+print("Parties with less than 1000 members are; ")
+party_members = {
+    "ANC": 2000,"DA": 1200,"EFF": 800,"IFP": 1100,
+    "UDM": 850,"COPE": 700,"ATM": 600,"NFP": 500,
+}
 
+# Filter out parties with less than 1000 members
+filtered_parties = [party for party in parties if party_members[party] <= 1000]
 
+print(filtered_parties)
 
+#2.5
+voters = [
+    {'name': 'sphamandla', 'registered': True},
+    {'name': 'Sthenjwa', 'registered': True},
+    {'name': 'Thabo', 'registered': True}
+   
+]
+
+registered_voters = list(filter(lambda voter: voter['registered'], voters))
+
+print(registered_voters)
+
+#2.6
+voters = [
+    {'name': 'Sphamandla', 'registered': True},
+    {'name': 'Sthenjwa', 'registered': False},
+    {'name': 'Thabo', 'registered': True}
+    
+]
+
+print("Using lambda functions")
+registered_voters = list(filter(lambda voter: voter['registered'], voters))
+
+print(registered_voters)
--- updateVoterInfo.py
+++ updateVoterInfo.py
@@ -8,17 +8,18 @@ def update_voter_info(voter_info, voter_id, name, voting_district, has_voted):
         print(f"Voter {voter_id} information updated.")
     else:
         # Add new voter's information if doesn't exist
-        voter_info[voter_id] = {
-            "name": name,
-            "voting_district": voting_district,
-            "has_voted": has_voted
+        voter_info[voter_id] = {"name": name,
+                                "voting_district": voting_district,
+                                "has_voted": has_voted
         }
-        print(f"New voter {voter_id} added.")
+        print(f"NewVoter {voter_id} added.")
 
 voter_info = {
-    "Voter": {"name": "John", "voting_district": "District A", "has_voted": False},
-    "Vvoter": {"name": "Alice", "voting_district": "District B", "has_voted": True}
+    "NewVoter221": {"name": "Sphamandla", "voting_district": "District A", "has_voted": False},
+    "NewVoter222": {"name": "Sthenjwa", "voting_district": "District B", "has_voted": True}
 }
 
-update_voter_info(voter_info, "V789", "Bob", "District C", False)
+update_voter_info(voter_info, "NewVoter223", "Thabo", "District C", False)
 print(voter_info)
+
+
